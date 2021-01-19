f8ab60c2df78988a928b369e06cb283a7f9477c1
Indra Octama <indra.octama@smartfren.com>
2021-01-19 16:34:39 +0700

    ssr example

---
+++ src/component/About.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+class About extends Component{
+
+    componentDidMount(){
+        //set active navbar for Products
+        document.getElementById('navbar-products').classList.add("active")
+      }
+
+    render(){
+        const products = ['Paket Internet 10GB','Paket Nelpon Hemat 5000','Paket Batman']
+        const listProducts = products.map((products,key)=>
+                <li className="list-group-item" key={key}>{products}</li>
+        );
+        return(
+            <div>
+                <h1>
+                    Product List
+                </h1>
+            <ul className="list-group">  
+                    {listProducts}
+            </ul>
+            </div>
+                
+        );
+    }
+}
+
+export default About;
